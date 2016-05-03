398ad1e6e424194f860e107f3430359beb07cf14
Anders Pearson <anders@columbia.edu>
2016-05-03 11:25:37 +0100

    :bookmark: 0.2.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-likertblock",
-    version="0.2.0",
+    version="0.2.1",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
