302c2fa4a0486080bf4352c61ded63d944503ad7
Anders Pearson <anders@columbia.edu>
2016-08-10 13:50:18 +0100

    django 1.10 style urls

--- likertblock/urls.py
+++ likertblock/urls.py
@@ -1,20 +1,19 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 from .views import (
     EditQuestionnaireView, DeleteQuestionView, ReorderQuestionsView,
     AddQuestionToQuestionnaireView, EditQuestionView,
 )
 
-urlpatterns = patterns(
-    'likertblock.views',
-    (r'^edit_questionnaire/(?P<pk>\d+)/$', EditQuestionnaireView.as_view(),
-     {}, 'edit-questionnaire'),
-    (r'^edit_questionnaire/(?P<pk>\d+)/add_question/$',
-     AddQuestionToQuestionnaireView.as_view(), {},
-     'add-question-to-questionnaire'),
-    (r'^edit_question/(?P<pk>\d+)/$', EditQuestionView.as_view(), {},
-     'likert-edit-question'),
-    (r'^delete_question/(?P<pk>\d+)/$', DeleteQuestionView.as_view(), {},
-     'likert-delete-question'),
-    (r'^reorder_questions/(?P<pk>\d+)/$', ReorderQuestionsView.as_view(), {},
-     'likert-reorder-questions'),
-)
+urlpatterns = [
+    url(r'^edit_questionnaire/(?P<pk>\d+)/$', EditQuestionnaireView.as_view(),
+        {}, 'edit-questionnaire'),
+    url(r'^edit_questionnaire/(?P<pk>\d+)/add_question/$',
+        AddQuestionToQuestionnaireView.as_view(), {},
+        'add-question-to-questionnaire'),
+    url(r'^edit_question/(?P<pk>\d+)/$', EditQuestionView.as_view(), {},
+        'likert-edit-question'),
+    url(r'^delete_question/(?P<pk>\d+)/$', DeleteQuestionView.as_view(), {},
+        'likert-delete-question'),
+    url(r'^reorder_questions/(?P<pk>\d+)/$', ReorderQuestionsView.as_view(),
+        {}, 'likert-reorder-questions'),
+]
